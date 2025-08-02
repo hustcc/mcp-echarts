bb5c50a97b46a6f6eb04a1c6e1da18967cbebf4a
2niuhe <carlton2tang@gmail.com>
2025-08-02 18:26:03 +0800

    fix: remove error logging on server start failure

--- src/index.ts
+++ src/index.ts
@@ -232,6 +232,5 @@ process.on("unhandledRejection", (reason, promise) => {
 
 // Start application
 main().catch((error) => {
-  console.error("Failed to start server:", error);
   process.exit(1);
 });
